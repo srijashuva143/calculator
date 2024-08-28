dbc91dcc2a40f998d7397995def5a9e8dec6839c
Sri_Jashuva <129363527+srijashuva143@users.noreply.github.com>
2024-08-28 21:30:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const result= document.querySelector(".result");
+const play= document.querySelector(".container");
+function love(){
+    play.style.display="block";
+ setTimeout(()=>{play.style.display="none";},1500);
+
+}
+
